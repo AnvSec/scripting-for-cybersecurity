e864c579b16fdf6c1dda84f426346114fcbc7c02
Anvil <91536377+AnvSec@users.noreply.github.com>
2026-09-14 11:09:09 +0000

    Complete Lab 1

---
+++ lab01/hello.py
@@ -0,0 +1,8 @@
+name = input ("What is your name?")
+course = input("What is your course?")
+fav_area = input("What is your favorite area in Cyber?")
+
+
+print ("Hello, "+ name + "!")
+print ("You are studying "+ course + "!")
+print ("You are interested in "+ fav_area + "!")
---
+++ lab01/student.py
@@ -0,0 +1,8 @@
+name = input ("What is your name?")
+studentID = input("What is your student ID?")
+collegeY = input("What year are you in?")
+
+
+print ("Name: "+ name + ".")
+print ("Student ID: "+ studentID + ".")
+print ("Year: "+ collegeY + ".")
